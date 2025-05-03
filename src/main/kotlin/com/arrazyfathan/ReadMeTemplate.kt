d71c2d3e4834de0f38b68cfe392a3e9f43796cd8
arrazyfathan <arrazy.rabbani266@gmail.com>
2025-05-03 21:09:37 +0700

    feat: remove about me section

--- src/main/kotlin/com/arrazyfathan/ReadMeTemplate.kt
+++ src/main/kotlin/com/arrazyfathan/ReadMeTemplate.kt
@@ -7,40 +7,6 @@ fun createReadMe(
 ![Ar Razy Fathan Rabbani Banner](https://github.com/arrazyfathan/arrazyfathan/blob/main/media/banner-dark.png#gh-dark-mode-only)
 ![Ar Razy Fathan Rabbani Banner](https://github.com/arrazyfathan/arrazyfathan/blob/main/media/banner-light.png#gh-light-mode-only)
 
-### <img width="30" alt="about" src="https://github.com/arrazyfathan/arrazyfathan/blob/main/media/about.png"> More about me
-
-```kotlin
-val Razy = human {
-    about {
-        name = "Ar Razy Fathan Rabbani"
-        role = "Android Engineer"
-    }
-
-    tech {
-        day("Kotlin", "Android")
-    }
-
-    links {
-        twitter = "@arrazyfathann"
-        website = "arrazyfathan.com"
-        linkedin = "linkedin.com/in/arrazyfathan"
-        email = "arrazy.rabbani266@gmail.com"
-        mastodon = "@arrazyfathan@androiddev.social"
-        instagram = "arrazyfathan"
-    }
-
-    more {
-      listOf(
-        "- 🔭 I’m currently working on Android/Kotlin ",
-        "- 👯 I’m looking to collaborate on Android projects ",
-        "- 💬 Ask me about Android and Kotlin ",
-        "- 📫 How to reach me: arrazy.rabbani266@gmail.com"
-      )
-    }
-}
-```
-
-
 <table><tr><td valign="top" width="100%">    
 
 ## GitHub Activity
